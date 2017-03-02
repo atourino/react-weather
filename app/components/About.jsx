d664b6e2a58ac3f2be329b45cdcc08f1f8c3da3d
Antonio Touriño <atourino@gmail.com>
2017-03-01 22:08:45 -0500

    Style about page.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>About pages rule.</p>
+      <h1 className="text-center">About</h1>
+      <p>A small little app to help me learn React.</p>
+      <p>
+        Tools used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - Javascript framework to build web apps.
+        </li>
+        <li>
+          <a href="https://www.apixu.com/">Apixu</a> - Third party service that provides the weather data.
+        </li>
+      </ul>
     </div>
   );
 };
